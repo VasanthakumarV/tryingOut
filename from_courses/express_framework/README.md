e278fd02328db860f127b61c6ffc070406325658
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2018-05-30 00:04:11 +0530

    add express getting started project

---
+++ from_courses/express_framework/README.md
@@ -0,0 +1,7 @@
+# Responsive Navbar
+
+This project will cover my learnings from treehouse express course
+
+## What does this project do?
+
+This project implements pug templating with custom error message for 404 error
---
+++ from_courses/express_framework/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', { name });
+    } else {
+        res.redirect('/hello');
+    }
+});
+
+app.get('/hello', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
+
+app.get('/cards', (req, res) => {
+    res.render('cards', { 
+        prompt: "Who is buried in Grant's tomb",
+        hint: "Think about whose tomb it is"
+     });
+});
+
+app.use((req, res, next) => {
+    const err = new Error();
+    err.message = "Not Found";
+    err.status = 500;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost 3000')
+});
