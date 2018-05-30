8a3fda90743bd4594e6030c1fd52d4bfd16a1c44
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2018-05-30 20:14:06 +0530

    Addn functionalities to the app

--- from_courses/express_framework/app.js
+++ from_courses/express_framework/app.js
@@ -3,59 +3,30 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.get('/cards', (req, res) => {
-    res.render('cards', { 
-        prompt: "Who is buried in Grant's tomb",
-        hint: "Think about whose tomb it is"
-     });
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-    const err = new Error();
-    err.message = "Not Found";
-    err.status = 500;
-    next(err);
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {
-    console.log('The application is running on localhost 3000')
+    console.log('The application is running on localhost:3000!')
 });
---
+++ from_courses/express_framework/routes/cards.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
+
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor(Math.random()*numberOfCards);
+    res.redirect(`/cards/${flashCardId}`);
+})
+
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+
+    if (!side) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    
+    const templateData = { text, id, name };
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+    } else if (side == 'answer') {
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+
+    }
+
+    res.render('cards', templateData);
+});
+
+module.exports = router;
